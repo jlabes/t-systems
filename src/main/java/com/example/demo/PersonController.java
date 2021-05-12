a8befbfae759ef24d80da8212ac568d6c88d61f7
f0fp1183 <f0fp1183@CTS1A519462.dc.com.br>
2021-05-12 12:57:44 -0300

    third commit

--- src/main/java/com/example/demo/PersonController.java
+++ src/main/java/com/example/demo/PersonController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -18,46 +19,58 @@ public class PersonController {
 
 	@Autowired
 	private PersonRepository repository;
-	
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
+
 		return "index";
 	}
-	
-	@RequestMapping(value="/person/register", method=RequestMethod.GET) 
-	public String registerPerson() {
+
+	@RequestMapping(value = "/person/register", method = RequestMethod.GET)
+	public String registerPerson(Model model) {
+		model.addAttribute("person", new Person());
 		return "registerPerson";
 	}
-	
+
 	@RequestMapping(value = "/people", method = RequestMethod.GET)
 	public String listAll(Model model) {
-		
+
 		List<Person> list = repository.findAll();
-		
+
 		if (list != null) {
 			model.addAttribute("people", list);
-		} 
-		
+		}
+
 		return "people";
 	}
-	
-	@RequestMapping(value="/delete/{personId}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/delete/{personId}", method = RequestMethod.GET)
 	public String deletePerson(@PathVariable long personId) {
-		
+
 		repository.deleteById(personId);
-		
+
 		return "redirect:/people";
 	}
-	
+
 	@RequestMapping(value = "/person/register", method = RequestMethod.POST)
-	public String addPerson(@ModelAttribute @Valid  Person person, Errors errors) {
-		
+	public String addPerson(@ModelAttribute @Valid Person person, Errors errors) {
+
 		if (errors.hasErrors()) {
 			return "registerPerson";
 		}
-		
+
 		repository.save(person);
+
+		return "redirect:/profile/" + person.getId();
+	}
+
+	@RequestMapping(value = "/profile/{personId}", method = RequestMethod.GET)
+	public String showPersonProfile(@PathVariable long personId, Model model) {
+
+		Person person = repository.getOne(personId);
 		
-		return "redirect:/people";
+		model.addAttribute("person", person);
+		
+		return "profile";
 	}
 }
