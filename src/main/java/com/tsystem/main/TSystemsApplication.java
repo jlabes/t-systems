8599cbe3bb530789b026f6a75d584a81408e6c63
f0fp1183 <f0fp1183@CTS1A519462.dc.com.br>
2021-05-12 22:23:22 -0300

    another commit

--- src/main/java/com/tsystem/main/TSystemsApplication.java
+++ src/main/java/com/tsystem/main/TSystemsApplication.java
@@ -2,8 +2,9 @@ package com.tsystem.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TSystemsApplication {
 
 	public static void main(String[] args) {
